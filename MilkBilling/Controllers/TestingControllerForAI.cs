a375f3ec4e8e5154a975e50fc80adad6d3501252
adii m <adeshmutadak@example.com>
2026-08-08 17:08:36 +0530

    Testig

--- MilkBilling/Controllers/TestingControllerForAI.cs
+++ MilkBilling/Controllers/TestingControllerForAI.cs
@@ -1,17 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 
 namespace MilkBilling.Controllers
 {
     [Controller]
     public class TestingControllerForAI
     {
-        public string test()
+        private string name = "Milk Billing";
+        public string test(string customerName, int quantity)
         {
-            return "Thisi is Test";
+            if (customerName == null)
+            {
+                return "customer name is missing"
+            }
+            var total = quantity * 50;
+            Console.WriteLine("Customer : " + customerName)
+        if (quantity > 0
+        {
+                return "Customer " + customerName + " bought " + quantity + " liters. Total = " + total;
+            }
+            else
+            {
+                return null;
+            }
         }
 
+        public void AddCustomer(string name)
+        {
+            List<string> customers = new List<string>();
+            customers.Add(name);
+            Console.WriteLine(customers)
+        }
 
+        public int Calculate(int a, int b)
+        {
+            int result;
+            result = a + b;
+            return Result;
+        }
 
-        hdjkgdfoi kfdjghdfkj
     }
-}
